c599b2abc2e4980ce82a41493052ed7529e7d625
yeominha <yeominha@gmail.com>
2026-07-30 02:47:18 +0900

    20260730_01

--- cpf-member/src/main/java/com/cpf/member/common/base/MbrBaseController.java
+++
@@ -1,7 +0,0 @@
-package com.cpf.member.common.base;
-
-import com.cpf.core.api.base.CpfBaseController;
-
-/** MBR 업무 Controller의 주제영역 공통 확장점입니다. */
-public abstract class MbrBaseController extends CpfBaseController {
-}
--- cpf-member/src/main/java/com/cpf/member/common/base/MbrBaseService.java
+++
@@ -1,7 +0,0 @@
-package com.cpf.member.common.base;
-
-import com.cpf.core.api.base.CpfBaseService;
-
-/** MBR 업무 Service의 주제영역 공통 확장점입니다. */
-public abstract class MbrBaseService extends CpfBaseService {
-}
